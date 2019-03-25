64bdbf6efe10a9ad7744b6ef66bd00a36432cd7c
Chuck Woodraska <chuck.woodraska@gmail.com>
2019-03-25 18:20:47 -0400

    Added click script for creating db. - CW

--- app/run.py
+++ app/run.py
@@ -1,8 +1,15 @@
 from app.app_factory import create_app
+from app.libs.utils import create_db
 import os
 
 config_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'config.ini')
 app = create_app(config_path)
+
+
+@app.cli.command("create_db")
+def create_database():
+    create_db()
+
 
 # debug = True
 # debug = False
